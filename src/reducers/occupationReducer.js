bdefb8dd99cab28f3b6649a2c9f6d64aecff9db3
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2022-01-14 21:54:29 -0800

    Created occupationReducer.

---
+++ src/reducers/occupationReducer.js
@@ -0,0 +1,32 @@
+export const initialState = {
+  data: null,
+  loading: false,
+  error: null,
+};
+
+export const occupationDataReducer = (state, action) => {
+  switch (action.type) {
+    case "CALL_API":
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case "SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        data: action.data,
+      };
+
+    case "ERROR":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+
+    default:
+      return state;
+  }
+};
